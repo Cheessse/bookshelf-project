e7ff70224be121ea72642c8521a5dfc306a33edc
Sakrid1s <bilinets.vitalik@gmail.com>
2024-03-03 01:50:44 +0200

    Update LS

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,26 +1,16 @@
 const SHOPPING_LIST_KEY = 'list';
 
-function toLocalStorage(key, value) {
-  const values = JSON.stringify(value);
-  localStorage.setItem(key, values);
+function addToShoppingList(book) {
+  const shopList = getAllBooks();
+  shopList.push(book);
+  localStorage.setItem(SHOPPING_LIST_KEY, JSON.stringify(shopList));
 }
 
-function getLocalStorageItem(key) {
-  const values = localStorage.getItem(key);
-  try {
-    return JSON.parse(values);
-  } catch (error) {
-    return console.log(error);
-  }
+function getAllBooks() {
+  return JSON.parse(localStorage.getItem(SHOPPING_LIST_KEY)) || [];
 }
 
-function onAddToListClick(book) {
-  const shoppingList = getLocalStorageItem();
-  shoppingList.push(book);
-  updateShoppingList(shoppingList);
-  //Повідомлення iziToast
-}
-
-function updateShoppingList(book) {
-  localStorage.setItem(SHOPPING_LIST_KEY, JSON.stringify(book));
-}
+export const localStorageAPI = {
+  addToShoppingList,
+  getAllBooks,
+};

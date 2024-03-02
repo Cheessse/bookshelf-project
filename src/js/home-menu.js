de3a262cada3ede06bffa26f509cfdc087aa5c52
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-03-02 09:54:36 +0100

    commit #4
    prettified JS, add html and CSS

--- src/js/home-menu.js
+++ src/js/home-menu.js
@@ -1,23 +1,21 @@
-// import { booksApi } from './home-books';
+import { booksApi } from './home-books';
 import { loadBooks } from './home-books';
+
 window.onload = function () {
-    const categoryList = document.getElementById("category-list"); //сылка на список категорий
+    const categoryList = document.getElementById("category-list");
     const title = document.createElement('li');
     title.textContent = "ALL CATEGORIES";
     title.id = "title";
     categoryList.appendChild(title);
-    booksApi().then(categories => { // Вызываем функцию booksApi, которая возвращает промис с категориями
-        categories.forEach(function (category) { // Проходимся по каждой категории
-            const paragraph = document.createElement('p'); // создаем <p> для кажд элем списка
+    booksApi().then(categories => {
+        categories.forEach(function (category) {
+            const paragraph = document.createElement('p');
             paragraph.textContent = category.list_name;
             paragraph.id = "p-list";
-
-            paragraph.addEventListener('click', function () {
-                loadBooks(category.list_name); //Вызываем функцию loadBooks при клике на элемент
-            });
-            categoryList.appendChild(paragraph); // Добавляем элемент в список внутри цикла
+            categoryList.appendChild(paragraph);
         });
     }).catch(error => {
         console.error('Error fetching data;', error);
     });
+    paragraph.addEventListener('click', loadBooks);
 };

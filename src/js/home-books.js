26471575bc241e84fcbd52850ac2f7cf5f33dff2
mariabykova23 <144001967+mariabykova23@users.noreply.github.com>
2024-03-02 10:35:57 +0400

    Update home-books.js

--- src/js/home-books.js
+++ src/js/home-books.js
@@ -1,18 +1,36 @@
-import {categoryList} from '../books API/books-api';
+// import { categoryList } from '../books API/books-api';
 
-import {booksByCategory} from '../books API/books-api';
+import { booksByCategory } from '../books API/books-api';
 
-import {topBooks} from '../books API/books-api';
+import { topBooks } from '../books API/books-api';
 
-import {renderBooksAllCat} from './render-function-books';
+import { renderBooksAllCat } from './render-function-books';
 
-import {renderBooksByCat} from './render-function-books';
+import { renderBooksByCat } from './render-function-books';
 
+const categoryBlock=document.querySelector('#category-list')
 
 let page;
 
-async function loadBooks(e){
-    e.preventDefault();
-    page=1;
+export async function loadBooks(e) {
+  e.preventDefault();
+  page = 1;
+  categoryName=categoryBlock.querySelectorAll('#p-list')
+  try {
+    const data = await booksByCategory();
+    const dataTop = await topBooks();
+    if (data.target.list_name === categoryName) {
+      renderBooksByCat(data);
+    } else {
+      renderBooksAllCat(dataTop);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 
+  e.target.reset();
 }
+
+// function loadMoreBooks() {
+//   page += 1;
+// }

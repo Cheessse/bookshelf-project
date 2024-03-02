4ef45377ecefd8acd48003936de87066a6614706
Chinwilla <bogdandrozd123@gmail.com>
2024-03-02 19:43:32 +0000

    Support_Ukraine_Run

--- src/js/home-ukraine.js
+++ src/js/home-ukraine.js
@@ -2,3 +2,10 @@ import Swiper from 'swiper';
 
 import { support } from './support';
 
+// const swiperOpt = {
+//     direction: 'vertical',
+//     navigation: {
+//         nextEl: 'swiper-btn-next'
+//     }
+// }
+

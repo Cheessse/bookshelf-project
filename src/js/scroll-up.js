270280f24c2f366d4f85323cf9e1d6451dcafee4
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-03-03 18:52:01 +0100

    Update scroll-up.js

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         scrollToTopBtn.style.display = "block";
     } else {

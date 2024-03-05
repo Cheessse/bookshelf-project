90e08d18976050bfe32a27c1f88d9013381793d4
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-03-05 20:08:07 +0100

    commit #15
    checked and clear code

--- src/js/home-menu.js
+++ src/js/home-menu.js
@@ -1,29 +1,4 @@
 
-// import { categoryList } from '../books API/books-api';
-// import { loadBooks } from './home-books';
-// const categoryBlock = document.querySelector("#category-list");
-// export default categoryBlock;
-
-// window.onload = function () {
-//     const myCategoryList = document.getElementById("category-list");
-//     const title = document.createElement('li');
-//     title.textContent = "ALL CATEGORIES";
-//     title.id = "title";
-//     myCategoryList.appendChild(title);
-//     categoryList().then(categories => {
-//         categories.forEach(function (category) {
-//             const paragraph = document.createElement('p'); // создаем <p> для кажд элем списка
-//             paragraph.textContent = category.list_name;
-//             paragraph.id = "p-list";
-//             paragraph.addEventListener('click', loadBooks); //Вызываем функцию loadBooks при клике на элемент
-//             myCategoryList.appendChild(paragraph);
-//         });
-//     }).catch(error => {
-//         console.error('Error fetching data;', error);
-//     });
-// };
-// ! ============================================================================================================*/
-
 import { categoryList } from '../books API/books-api';
 import { loadBooks } from './home-books';
 import { loadBooksAllCat } from './home-books';
@@ -35,16 +10,14 @@ window.onload = function () {
     const title = document.createElement('li');
     title.textContent = 'ALL CATEGORIES';
     title.id = 'title';
-    // ! ===================================================
     title.addEventListener('click', function () {
         window.location.href = './index.html'; // Перенаправление на главную страницу при клике на ALL CATEGORIES
     });
-    // ! ===================================================
     myCategoryList.appendChild(title);
     categoryList()
         .then(categories => {
             categories.forEach(function (category) {
-                const paragraph = document.createElement('p'); // создаем <p> для кажд элем списка
+                const paragraph = document.createElement('p');
                 paragraph.textContent = category.list_name;
                 paragraph.classList.add('p-list');
                 myCategoryList.appendChild(paragraph);

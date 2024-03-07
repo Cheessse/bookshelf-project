9ee5b4599f86e87f41501e6ff1f828eb1dd89558
David <143638937+Cheessse@users.noreply.github.com>
2024-03-07 23:45:38 +0000

    upd file

--- src/books API/books-api.js
+++ src/books API/books-api.js
@@ -11,31 +11,6 @@ export async function categoryList() {
   return res.data;
 }
 
-// export async function booksByCategory(category) {
-//   const URL = 'https://books-backend.p.goit.global/books/category';
-
-//   const options = {
-//     method: 'GET',
-//     params: {
-//       category: category,
-//     },
-//   };
-
-//   const res = await axios.get(URL, { options });
-//   return res.data;
-// }
-
-// export async function topBooks() {
-//   const URL = 'https://books-backend.p.goit.global/books/top-books';
-
-//   const options = {
-//     method: 'GET',
-//   };
-
-//   const res = await axios.get(URL, { options });
-//   return res.data;
-// }
-
 export async function booksId() {
   const URL = 'https://books-backend.p.goit.global/books/{id}';
 
--- src/main.js
+++ src/main.js
@@ -3,7 +3,4 @@ import './js/home-books';
 import './js/home-menu';
 import './js/home-ukraine';
 import './js/modal-window';
-// import './js/pagination';
-import './js/loader';
-import './js/scroll-up';
 import './js/dark-theme';

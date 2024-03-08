8707e8e5d177722167eb3ad55774bac88177cb56
Sakrid1s <bilinets.vitalik@gmail.com>
2024-03-08 02:16:20 +0200

    upd

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,20 +1,20 @@
 const slider = document.querySelector('#slider');
 const themeStyle = document.querySelector('#theme-style');
 
-let savedTheme = localStorage.getItem('theme');
+const savedTheme = localStorage.getItem('theme');
 
 // Встановлюємо тему за замовчуванням або збережену тему
 if (savedTheme) {
   themeStyle.setAttribute('href', `./css/${savedTheme}.css`);
+} else {
+  themeStyle.setAttribute('href', `./css/styles-light-theme.css`);
 }
 
-// function to set the theme
 function setTheme(themeName) {
   localStorage.setItem('theme', themeName);
   themeStyle.setAttribute('href', `./css/${themeName}.css`);
 }
 
-// function to toggle between light and dark theme
 function toggleTheme() {
   if (slider.checked) {
     setTheme('styles-light-theme');
@@ -25,7 +25,6 @@ function toggleTheme() {
 
 slider.addEventListener('change', toggleTheme);
 
-// Immediately invoked function to set the theme on initial load
 function checkTheme() {
   if (savedTheme === 'styles-dark-theme') {
     setTheme('styles-dark-theme');

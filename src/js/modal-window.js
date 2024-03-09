d858303ce3c3e5da411dc320eed3170b7e31e65b
AntoninaMed <ton.kraski2427@gmail.com>
2024-03-09 23:15:08 +0200

    upd

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -62,7 +62,6 @@ function addBook(id) {
   localStorage.setItem('idBooks', JSON.stringify(idBooks));
   btn.textContent = 'Remove from the shopping list';
   addMessage.hidden = false;
-  updateShoppingList();
 }
 
 function removeBook(id) {
@@ -72,7 +71,6 @@ function removeBook(id) {
   localStorage.setItem('idBooks', JSON.stringify(idBooks));
   btn.textContent = 'Add to shopping list';
   addMessage.hidden = true;
-  updateShoppingList();
 }
 
 function checkLocalStorage(book) {

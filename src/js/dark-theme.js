5668906d32146ec581aa7b899ad1500155927a64
Sakrid1s <bilinets.vitalik@gmail.com>
2024-03-09 18:07:51 +0200

    upd

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,50 +1,3 @@
-// const slider = document.querySelector('#slider');
-// let themeStyle = document.querySelector('#theme-style');
-
-// const savedTheme = localStorage.getItem('theme');
-
-// if (!themeStyle) {
-//   themeStyle = document.createElement('link');
-//   themeStyle.id = 'theme-style';
-//   document.head.appendChild(themeStyle);
-// }
-
-// // Встановлюємо тему за замовчуванням або збережену тему
-// if (!savedTheme) {
-//   themeStyle.setAttribute('href', `./css/styles-light-theme.css`);
-//   // themeStyle.setAttribute('href', `./css/${savedTheme}.css`);
-// } else {
-//   themeStyle.setAttribute('href', `./css/${savedTheme}.css`);
-//   // themeStyle.setAttribute('href', `./css/styles-light-theme.css`);
-// }
-
-// function setTheme(themeName) {
-//   localStorage.setItem('theme', themeName);
-//   themeStyle.setAttribute('href', `./css/${themeName}.css`);
-// }
-
-// function toggleTheme() {
-//   if (slider.checked) {
-//     setTheme('styles-light-theme');
-//   } else {
-//     setTheme('styles-dark-theme');
-//   }
-// }
-
-// slider.addEventListener('change', toggleTheme);
-
-// function checkTheme() {
-//   if (savedTheme === 'styles-dark-theme') {
-//     setTheme('styles-dark-theme');
-//     slider.checked = false;
-//   } else {
-//     setTheme('styles-light-theme');
-//     slider.checked = true;
-//   }
-// }
-
-// checkTheme();
-
 const slider = document.querySelector('#slider');
 let themeStyle = document.querySelector('#theme-style');
 
@@ -90,7 +43,7 @@ slider.addEventListener('change', toggleTheme);
 
 const lightTheme = {
   headerColor: '#fff',
-  bodyColor: '#e0e0e0',
+  bodyColor: '#f6f6f6',
   yellow: '#eac645',
   violet: '#4f2ee8',
   fontColor: '#111',

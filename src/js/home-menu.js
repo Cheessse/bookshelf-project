2d2521090be900b7e30a6d5392faac442cfba0d3
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-03-10 11:29:17 +0100

    commit #24
    Change of #title style on hover over ‘.p-list’

--- src/js/home-menu.js
+++ src/js/home-menu.js
@@ -18,11 +18,32 @@ window.onload = function () {
                 paragraph.classList.add('p-list');
                 myCategoryList.appendChild(paragraph);
             });
+            // смена стиля при hover на '.p-list'
+            let pListElements = document.querySelectorAll('.p-list');
+            let titleElement = document.querySelector('#title');
+            pListElements.forEach((pElement) => {
+                pElement.addEventListener('mouseover', function () {
+                    titleElement.style.fontWeight = '400';
+                    titleElement.style.fontSize = '16px';
+                    titleElement.style.lineHeight = '112%';
+                    titleElement.style.letterSpacing = '-0.02em';
+                    titleElement.style.color = 'var(--all-categories-list-text)';
+                });
+                //Возврат #title к исходным значениям
+                pElement.addEventListener('mouseout', function () {
+                    titleElement.style.fontWeight = '700';
+                    titleElement.style.fontSize = '18px';
+                    titleElement.style.lineHeight = '133%';
+                    titleElement.style.letterSpacing = '-0.02em';
+                    titleElement.style.color = 'var(--all-categories-title-and-hover)';
+                });
+            });
         })
         .catch(error => {
             console.error('Error fetching data;', error);
         });
     loadBooksAllCat(category);
+
 };
 
 categoryBlock.addEventListener('click', async event => {
